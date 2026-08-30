34e966610f85aab8454b6387150681a337de4051
yeontj <jungyeontj@gmail.com>
2026-08-30 15:42:00 -0500

    TEST UPLOAD - THIS IS SO HARD
    I don't understand why this has to be so inconveinent.
    But I guess this is how it is done. Once you learn it, you get used to it.

---
+++ RI-CLPM.R
@@ -0,0 +1,74 @@
+##########################################
+#RI-CLPM##################################
+#FRIEND SUPPORT X LONELINESS##############
+#MASTER'S THESIS #########################
+#START DATE: 08.28.2026. #################
+#JUNGYEON SUH ############################
+##########################################
+
+library('lavaan')
+
+#read data
+alive = readRDS('thesisdata_alive.rds')
+
+alive
+
+RICLPM_3wave <- '
+  # Between-person components
+  RI_lon =~ 1*loneliness11.1 +
+            1*loneliness11.2 +
+            1*loneliness11.3
+
+  RI_friend =~ 1*frsupport1 +
+               1*frsupport2 +
+               1*frsupport3
+
+  # Within-person components
+  wlon1 =~ 1*loneliness11.1
+  wlon2 =~ 1*loneliness11.2
+  wlon3 =~ 1*loneliness11.3
+
+  wfriend1 =~ 1*frsupport1
+  wfriend2 =~ 1*frsupport2
+  wfriend3 =~ 1*frsupport3
+
+  # Freely estimated lagged effects
+  wlon2 + wfriend2 ~ wlon1 + wfriend1
+  wlon3 + wfriend3 ~ wlon2 + wfriend2
+
+  # Initial within-person covariance
+  wlon1 ~~ wfriend1
+
+  # Innovation covariances
+  wlon2 ~~ wfriend2
+  wlon3 ~~ wfriend3
+
+  # Random-intercept variances and covariance
+  RI_lon ~~ RI_lon
+  RI_friend ~~ RI_friend
+  RI_lon ~~ RI_friend
+
+  # Within-person variances
+  wlon1 ~~ wlon1
+  wfriend1 ~~ wfriend1
+
+  # Residual within-person variances
+  wlon2 ~~ wlon2
+  wfriend2 ~~ wfriend2
+  wlon3 ~~ wlon3
+  wfriend3 ~~ wfriend3
+'
+
+fit_free <- lavaan(
+  RICLPM_3wave,
+  data = alive,
+  missing = "fiml",
+  estimator = "MLR",
+  meanstructure = TRUE,
+  int.ov.free = TRUE
+)
+
+summary(fit_free, standardized = F)
+summary(fit_free, standardized = T)
+
+
